4b929821abbff4fb4a5beb9f049f235f26066306
AlirezaEbadi <ebadia256@gmail.com>
2023-02-09 15:10:28 +0330

    complete product Slider

--- components/ProductSlideBox/index.js
+++ components/ProductSlideBox/index.js
@@ -5,26 +5,34 @@ import { RiShoppingCartLine } from "react-icons/ri";
 
 const index = () => {
   return (
-    <article className="flex flex-col bg-white justify-around w-[19rem] h-[20rem] rounded-2xl shadow-[5px_5px_8px_rgba(0,0,0,0.25)] transition-all duration-500">
-      <div className="flex items-center justify-center">
-        <Image className="rounded-2xl" alt="productSlideImage" width={280} height={140} src={"/images/productSlider/ganj.jpg"} />
-      </div>
-      <div className="flex flex-col items-start">
-        <div className="mr-3">
-          <h3 className="text-[#656666] font-bold text-lg">کتاب گنج-نوشته جلال آل احمد</h3>
-          <div className="flex items-center gap-x-2 mt-4 mb-6">
-            <p className="py-2 px-3 bg-[#F4F4F5] rounded-lg">رمان</p>
-            <p className="py-2 px-3 bg-[#F4F4F5] rounded-lg">الیف شافاک</p>
-          </div>
+    <article className="sliderItem p-2">
+      <div className="flex flex-col bg-white justify-around w-[19rem] h-[20rem] rounded-2xl shadow-[5px_5px_8px_rgba(0,0,0,0.25)] hover:shadow-[5px_5px_15px_rgba(0,0,0,0.7)] transition-all duration-500">
+        <div className="flex items-center justify-center">
+          <Image
+            className="rounded-2xl"
+            alt="productSlideImage"
+            width={280}
+            height={140}
+            src={"/images/productSlider/ganj.jpg"}
+          />
         </div>
-        <div className="flex items-center justify-between w-full">
-          <div className="flex items-center gap-x-2 mr-3">
-            <VscSearch className="p-2 w-10 h-10 rounded-lg bg-[#E4E4E7]" />
-            <BiSave className="p-2 w-10 h-10 rounded-lg bg-[#E4E4E7]" />
+        <div className="flex flex-col items-start">
+          <div className="mr-3">
+            <h3 className="text-[#656666] font-bold text-lg">کتاب گنج-نوشته جلال آل احمد</h3>
+            <div className="flex items-center gap-x-2 mt-4 mb-6">
+              <p className="py-2 px-3 bg-[#F4F4F5] rounded-lg">رمان</p>
+              <p className="py-2 px-3 bg-[#F4F4F5] rounded-lg">الیف شافاک</p>
+            </div>
           </div>
-          <div className="flex items-center justify-end w-full gap-x-2">
-            <RiShoppingCartLine className="bg-[#FB923C] p-2 w-10 h-10 rounded-lg text-white" />
-            <p className="bg-[#A3A3A3] px-5 py-2 rounded-tr-lg rounded-br-lg text-white font-bold">2000 تومان</p>
+          <div className="flex items-center justify-between w-full">
+            <div className="flex items-center gap-x-2 mr-3">
+              <VscSearch className="p-2 w-10 h-10 rounded-lg bg-[#E4E4E7]" />
+              <BiSave className="p-2 w-10 h-10 rounded-lg bg-[#E4E4E7]" />
+            </div>
+            <div className="flex items-center justify-end w-full gap-x-2">
+              <RiShoppingCartLine className="bg-[#FB923C] p-2 w-10 h-10 rounded-lg text-white" />
+              <p className="bg-[#A3A3A3] px-5 py-2 rounded-tr-lg rounded-br-lg text-white font-bold">2000 تومان</p>
+            </div>
           </div>
         </div>
       </div>
--- components/ProductSlider/index.js
+++ components/ProductSlider/index.js
@@ -1,26 +1,50 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client";
 import ProductSlideBox from "../ProductSlideBox";
 import { HiOutlineChevronRight } from "react-icons/hi";
 import { HiOutlineChevronLeft } from "react-icons/hi";
+import { useRef } from "react";
 const index = () => {
+  const sliderRef = useRef();
+  const swiperCustome = (data) => {
+    if (sliderRef.current) {
+        const width = sliderRef.current.offsetWidth;
+        sliderRef.current.scrollTo(
+            sliderRef.current.scrollLeft+width*data,0
+        )
+    }
+  };
   return (
     <section className="container mx-auto px-10 py-5 bg-[#818CF8]">
       <div className="flex items-center justify-between">
         <div className="border-r-4 text-white border-white pr-2 font-bold text-2xl border-solid">
           <h3>اپلیکیشن ها</h3>
         </div>
-        <div className="flex text-2xl gap-x-4">
-          <HiOutlineChevronRight className="bg-white text-[#5a51ff] w-10 h-10" />
-          <HiOutlineChevronLeft className="bg-white" />
+        <div className="flex justify-around items-center w-[15rem]">
+          <div className="flex gap-x-4 text-2xl">
+            <HiOutlineChevronRight onClick={()=>swiperCustome(1)} className="bg-white text-[#0000ff] rounded-lg w-10 h-10 hover:bg-[#0000ff] hover:text-white transition-all duration-500 cursor-pointer" />
+            <HiOutlineChevronLeft onClick={()=>swiperCustome(-1)} className="bg-white text-[#0000ff] rounded-lg w-10 h-10 hover:bg-[#0000ff] hover:text-white transition-all duration-500 cursor-pointer" />
+          </div>
+          <p className="px-5 py-2 bg-[#F97316] text-white border-2 rounded-lg border-white border-solid font-bold transition-all duration-500 hover:bg-[#0000b2] hover:text-white cursor-pointer">
+            دیدن همه
+          </p>
         </div>
       </div>
-      <div className="flex items-center justify-start gap-8 flex-wrap py-8">
-        <ProductSlideBox />
-        <ProductSlideBox />
-        <ProductSlideBox />
-        <ProductSlideBox />
-        <ProductSlideBox />
-        <ProductSlideBox />
-        <ProductSlideBox />
+      <div ref={sliderRef} className="sliderContainer overflow-x-scroll">
+        <div className="flex items-center justify-start gap-5 py-8 mx-10">
+          <ProductSlideBox />
+          <ProductSlideBox />
+          <ProductSlideBox />
+          <ProductSlideBox />
+          <ProductSlideBox />
+          <ProductSlideBox />
+          <ProductSlideBox />
+          <ProductSlideBox />
+          <ProductSlideBox />
+          <ProductSlideBox />
+          <ProductSlideBox />
+          <ProductSlideBox />
+        </div>
       </div>
     </section>
   );
